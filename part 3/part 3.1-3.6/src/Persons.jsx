52a42003a1ffa6550d24282704d1b6edf5208a9a
gladyreigh <tgladyreigh@gmail.com>
2024-08-05 07:22:02 +0400

    Add files via upload

---
+++ part 3/part 3.1-3.6/src/Persons.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Persons = ({ persons, handleDeletePerson }) => {
+  return (
+    <div>
+      {persons.map(person => (
+        <div key={person.id}>
+          {person.name} {person.number}
+          <button onClick={() => handleDeletePerson(person.id)}>delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Persons;
